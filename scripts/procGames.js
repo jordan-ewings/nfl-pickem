e88eb8463a3b0c1780e7da20a08d1cf8e6166f1d
Jordan Ewings <jordanewings@outlook.com>
2023-12-04 09:55:30 -0600

    gen

--- scripts/procGames.js
+++ scripts/procGames.js
@@ -36,3 +36,87 @@ function procGames(dbGames, dbResponses) {
 
   return weeklydata;
 }
+
+/* ------------------------------------------------ */
+
+async function fetchLive(week) {
+
+  console.log('FETCHING LIVE');
+  const resp = await fetch('https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard?seasontype=2&dates=2023&week=' + week);
+  const raw = await resp.text();
+  const events = JSON.parse(raw)['events'];
+
+  const proc = events.map((game) => {
+
+    let comp = game.competitions[0];
+    let hid = comp.competitors[0].id;
+    let aid = comp.competitors[1].id;
+
+    let data = {};
+    data.game_id = game.id;
+    data.week = game.week.number;
+    data.home_score = game.competitions[0].competitors[0].score;
+    data.away_score = game.competitions[0].competitors[1].score;
+    data.state = game.status.type.state;
+    data.stateshort = game.status.type.shortDetail;
+    data.home_poss = '0';
+    data.away_poss = '0';
+
+    if (comp.situation) {
+      if (comp.situation.possession) {
+        let pid = comp.situation.possession;
+        if (pid == hid) data.home_poss = '1';
+        if (pid == aid) data.away_poss = '1';
+        data.poss_loc = comp.situation.possessionText;
+        data.poss_dd = comp.situation.shortDownDistanceText;
+      }
+    }
+
+    return data;
+  });
+
+  console.log(proc);
+  return proc;
+}
+
+/* ------------------------------------------------ */
+
+function updateData(proc, live) {
+  console.log('UPDATING LIVE');
+  let week = live[0].week.toString();
+  let data = proc.map((w) => {
+    if (w.week == week) {
+      w.games = w.games.map((game) => {
+        let g = game;
+        let glive = live.filter((x) => x.game_id == g.game_id)[0];
+        console.log(g);
+        console.log(glive);
+        g.away_score = glive.away_score;
+        g.home_score = glive.home_score;
+        g.state = glive.state;
+        g.stateshort = glive.stateshort;
+        g.away_poss = glive.away_poss;
+        g.home_poss = glive.home_poss;
+        g.poss_loc = glive.poss_loc;
+        g.poss_dd = glive.poss_dd;
+        let ascore = parseInt(glive.away_score);
+        let hscore = parseInt(glive.home_score);
+
+        if (g.state == 'post') {
+          if (ascore > hscore) {
+            g.win_team = g.away_team;
+          } else if (hscore > ascore) {
+            g.win_team = g.home_team;
+          } else {
+            g.win_team = 'TIE';
+          }
+        }
+        return g;
+      });
+    }
+    return w;
+  });
+
+  // console.log(data);
+  return data;
+}
