ca1be9c3859de552292c5bfb5ff5091e3cc37d65
Jordan Ewings <jordanewings@outlook.com>
2023-12-14 13:30:15 -0600

    gen

---
+++ scripts/chart.js
@@ -0,0 +1,91 @@
+
+function getChartOptions() {
+
+  let colors = {
+    defaultText: 'rgb(255, 255, 255, .85)',
+    legendText: 'rgb(255, 255, 255, .75)',
+    axisText: 'rgb(255, 255, 255, .6)',
+    gridLine: 'rgb(255, 255, 255, .05)',
+    tooltipBg: 'rgba(0, 0, 0, .8)',
+    tooltipTitle: 'rgba(255, 255, 255, .85)',
+    tooltipBody: 'rgba(255, 255, 255, .85)',
+  };
+
+  let fonts = {
+    default: { size: 12, weight: '500' },
+    legend: { size: 14, weight: '500' },
+    axis: { size: 12, weight: '500' },
+    tooltipTitle: { size: 14, weight: '500' },
+    tooltipBody: { size: 12, weight: '500' },
+  };
+
+  let title = {};
+  title.display = false;
+
+  let legend = {};
+  legend.display = true;
+  legend.position = 'top';
+  legend.align = 'center';
+  legend.labels = {};
+  legend.labels.color = colors.legendText;
+  legend.labels.padding = 20;
+  legend.labels.usePointStyle = true;
+  legend.labels.font = fonts.legend;
+  legend.labels.pointStyle = 'rect';
+
+  let x = {};
+  x.offset = true;
+  x.grid = {};
+  x.grid.display = false;
+  x.grid.color = colors.gridLine;
+  x.ticks = {};
+  x.ticks.color = colors.axisText;
+  x.ticks.padding = 8;
+  x.ticks.maxRotation = 90;
+  x.ticks.minRotation = 0;
+  x.border = {};
+  x.border.display = false;
+
+  let y = {};
+  y.offset = true;
+  y.grid = {};
+  y.grid.color = colors.gridLine;
+  y.grid.drawTicks = false;
+  y.grid.drawBorder = false;
+  y.ticks = {};
+  y.ticks.color = colors.axisText;
+  y.ticks.padding = 0;
+  y.border = {};
+  y.border.display = false;
+
+  let tooltip = {};
+  tooltip.enabled = true;
+  tooltip.backgroundColor = colors.tooltipBg;
+  tooltip.titleColor = colors.tooltipTitle;
+  tooltip.bodyColor = colors.tooltipBody;
+  tooltip.titleFont = fonts.tooltipTitle;
+  tooltip.bodyFont = fonts.tooltipBody;
+  tooltip.padding = 8;
+  tooltip.cornerRadius = 4;
+  tooltip.borderWidth = 0;
+
+  let options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    interaction: {
+      mode: 'x',
+      intersect: false,
+    },
+    plugins: {
+      title: title,
+      legend: legend,
+      tooltip: tooltip,
+    },
+    scales: {
+      x: x,
+      y: y,
+    },
+  };
+
+  return options;
+}
---
+++ scripts/procStats.js
@@ -0,0 +1,35 @@
+/* ------------------------------------------------ */
+
+function procStats(PlayerStats) {
+  let raw = PlayerStats;
+  raw.forEach((x) => {
+    let fields = Object.keys(x);
+    let fieldsStr = ['player', 'rank'];
+    let fieldsInt = fields.filter((f) => !fieldsStr.includes(f));
+    let fieldsPct = fields.filter((f) => f.includes('pct'));
+    fieldsInt = fieldsInt.filter((f) => !fieldsPct.includes(f));
+    fieldsInt.forEach((f) => {
+      x[f] = parseInt(x[f]);
+    });
+    fieldsPct.forEach((f) => {
+      x[f] = parseFloat(x[f]);
+    });
+  });
+
+  // let fields = Object.keys(raw[0]);
+  // let wkFields = fields.filter((f) => f.includes('wk_'));
+  // wkFields.forEach((f) => {
+  //   let cumField = f.replace('wk_', '');
+  //   let newCumFieldName = 'cum_' + cumField;
+  //   raw.forEach((x) => {
+  //     x[newCumFieldName] = x[cumField];
+  //     x[cumField] = x[f];
+  //     delete x[f];
+  //   });
+  // });
+
+  let data = raw;
+
+  return data;
+}
+
