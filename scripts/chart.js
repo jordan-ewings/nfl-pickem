de9a88864d650b06eb6a04e69dbd9bd6f1c0b304
Jordan Ewings <jordanewings@outlook.com>
2023-12-15 18:40:04 -0600

    gen

--- scripts/chart.js
+++ scripts/chart.js
@@ -19,6 +19,30 @@ function getChartOptions() {
     tooltipBody: { size: 12, weight: '500' },
   };
 
+  // set default line/point styles
+  let c = {
+    color: 'rgba(10, 132, 255, 1)',
+    colorAlpha: 'rgba(10, 132, 255, .2)',
+  };
+
+  // set default element styles
+  let elements = {};
+  elements.line = {
+    fill: false,
+    borderWidth: 3,
+    tension: 0.4,
+    borderColor: c.color,
+    backgroundColor: c.colorAlpha,
+  };
+
+  elements.point = {
+    radius: 3,
+    borderWidth: 1,
+    hoverRadius: 6,
+    borderColor: c.color,
+    backgroundColor: c.color,
+  };
+
   let title = {};
   title.display = false;
 
@@ -35,30 +59,54 @@ function getChartOptions() {
 
   let x = {};
   x.offset = true;
+  x.border = {};
+  x.border.display = false;
   x.grid = {};
-  x.grid.display = false;
+  x.grid.display = true;
+  x.grid.drawOnChartArea = false;
   x.grid.color = colors.gridLine;
+  x.grid.drawTicks = true;
+  x.grid.drawBorder = false;
   x.ticks = {};
   x.ticks.color = colors.axisText;
   x.ticks.padding = 8;
   x.ticks.maxRotation = 90;
-  x.ticks.minRotation = 0;
-  // x.ticks.includeBounds = false;
-  x.border = {};
-  x.border.display = false;
+  x.ticks.minRotation = 90;
+
 
   let y = {};
-  y.offset = true;
+  y.position = 'left';
+  y.reverse = false;
+  y.border = {};
+  y.border.display = false;
   y.grid = {};
   y.grid.color = colors.gridLine;
   y.grid.drawTicks = false;
   y.grid.drawBorder = false;
   y.ticks = {};
   y.ticks.color = colors.axisText;
-  y.ticks.padding = 0;
+  y.ticks.padding = 3;
   y.ticks.includeBounds = false;
-  y.border = {};
-  y.border.display = false;
+
+  let y1 = JSON.parse(JSON.stringify(y));
+  y1.position = 'right';
+  y1.display = 'auto';
+  y1.grid.drawOnChartArea = false;
+
+  let ticksCallback = function (value, index, ticks) {
+    let vals = ticks.map((x) => x.value);
+    let min = Math.min(...vals);
+    let max = Math.max(...vals);
+    let isPctData = min >= 0 && max <= 1;
+    if (isPctData) {
+      return Math.round(value * 100) + '%';
+    } else {
+      return value;
+    }
+  };
+
+  y.ticks.callback = ticksCallback;
+  y1.ticks.callback = ticksCallback;
 
   let tooltip = {};
   tooltip.enabled = true;
@@ -72,12 +120,32 @@ function getChartOptions() {
   tooltip.borderWidth = 0;
   tooltip.boxPadding = 3;
   tooltip.bodySpacing = 3;
+  tooltip.itemSort = function (a, b, data) {
+    let meta = DATA.Chart.live;
+    if (meta.mode == 'byPlayer') {
+      let field = meta.fields[0];
+      if (field.reverse) return b.parsed - a.parsed;
+      if (!field.reverse) return a.parsed - b.parsed;
+    } else if (meta.mode == 'byStat') {
+      if (a.label < b.label) return -1;
+      if (a.label > b.label) return 1;
+      return 0;
+    }
+  };
+  tooltip.callbacks = {};
+  tooltip.callbacks.label = function (context) {
+    let name = context.dataset.label;
+    let value = context.dataset.data[context.dataIndex].display;
+    let label = name + ': ' + value;
+    return label;
+  };
 
   let options = {
     responsive: true,
     maintainAspectRatio: false,
+    elements: elements,
     interaction: {
-      mode: 'x',
+      mode: 'index',
       intersect: false,
     },
     plugins: {
@@ -88,6 +156,7 @@ function getChartOptions() {
     scales: {
       x: x,
       y: y,
+      y1: y1,
     },
   };
 
